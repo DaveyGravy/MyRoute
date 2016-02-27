e04797a74ff1c4f681f4827215b89cd16d2e40be
Dave <david@davids-imac.local>
2016-02-27 14:00:59 -0500

    Initial commit of iOS project for PlowPlow - 2 tab bar interface - 1st take is the route's view for plow route listings

--- MyRoute/FirstViewController.m
+++ MyRoute/FirstViewController.m
@@ -17,11 +17,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // setup the array holding the route data
+    routeData = [[NSArray alloc] initWithObjects:   @"101 Richmond Rd",
+                                                    @"299 Elgin Street West",
+                                                    @"360 Patricia St",
+                                                    @"24 Sussex",
+                                                    @"1111 Milky Way",
+                                                    @"55 Main St",
+                                                    nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark TableView stuff
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [routeData count];
+}
+
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [routeTableView dequeueReusableCellWithIdentifier:@"Route Cells"];
+    cell.textLabel.text = [routeData objectAtIndex:indexPath.row];
+    
+    cell.imageView.image = [UIImage imageNamed:@"house"];
+    
+    return cell;
 }
 
 @end
